310766818f6fb1625e6e970d68347edcdb45270f
Anthony Ferlazzo <anthony.ferlazzo@infostretch.com>
2012-04-16 11:39:03 -0700

    messaging working. Content ask color of icon when loading

--- GCvisitInvite.js
+++ GCvisitInvite.js
@@ -1,4 +1,4 @@
-console.log('visitInvite.js start');
+console.log('content 1 - visitInvite.js start');
 
 // Content scripts are JavaScript files that run in the context of web pages.
 
@@ -11,10 +11,12 @@ console.log('visitInvite.js start');
 // This connect method attempts to connect to other listeners, primarily used by 
 // content scripts connection to other parts of the extension, and assigns a port to 
 // be used for this particular communication
-
+/*
 // In order to handle incoming connections set up a onConnect event listener
 console.log('setting up chrome.extension.onConnect.addListener callback function');
+
 chrome.extension.onConnect.addListener(function(port) {
+	console.assert(port.name == "activeOrNot");
 	// once a connect request is received, define a listener to process the messages
 	port.onMessage.addListener(function(msg) {
 		if (msg.colorIs == 'green'){
@@ -25,37 +27,63 @@ chrome.extension.onConnect.addListener(function(port) {
 		console.log('in content script. msg.colorIs: '+msg.colorIs); 
 	});
 });
+*/
 
+// send a request message to background task
+chrome.extension.sendRequest({greeting: "hello"}, function(response) {
+  console.log(response.farewell);
+});
+
+var iconColor = 'red';	// by default assume the extension background preocess is not loaded
+// send a request message to background task for iconColor
+chrome.extension.sendRequest({greeting: "iconColor"}, function(response) {
+	iconColor = response.farewell;
+	console.log('icon color is ' + iconColor);
+	if (iconColor === 'green'){
+		injectCode();
+	}
+});
+// listen for requests
+chrome.extension.onRequest.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+});  
 //----------------------------------
 
-var articleParent = document.getElementById('maindetails_center_bottom');
-var myArticle = document.createElement('div');
+function injectCode() {
+	var articleParent = document.getElementById('maindetails_center_bottom');
+	var myArticle = document.createElement('div');
 
-// Get a reference to the first child and in sert our article here
-var theFirstChild = articleParent.firstChild;
-articleParent.insertBefore(myArticle, theFirstChild);
-// Add attributes to the new article
-myArticle.setAttribute('id', 'saapArticle');
-myArticle.setAttribute('class', 'article');
+	// Get a reference to the first child and in sert our article here
+	var theFirstChild = articleParent.firstChild;
+	articleParent.insertBefore(myArticle, theFirstChild);
+	// Add attributes to the new article
+	myArticle.setAttribute('id', 'saapArticle');
+	myArticle.setAttribute('class', 'article');
 
-$('#saapArticle').html("<h2>Visit your favorite places in <span>Ocean's Eleven</span></h2> \
-<ul id='saapSlider'> \
-  <li> \
-    <div id='pic1'></div> \
-  </li> \
-  <li> \
-    <div id='pic2'></div> \
-  </li> \
-  <li> \
-    <div id='pic3'></div> \
-  </li> \
-  <li> \
-     <div id='pic4'></div> \
-  </li> \
-</ul>");
+	$('#saapArticle').html("<h2>Visit your favorite places in <span>Ocean's Eleven</span></h2> \
+	<ul id='saapSlider'> \
+	  <li> \
+		<div id='pic1'></div> \
+	  </li> \
+	  <li> \
+		<div id='pic2'></div> \
+	  </li> \
+	  <li> \
+		<div id='pic3'></div> \
+	  </li> \
+	  <li> \
+		 <div id='pic4'></div> \
+	  </li> \
+	</ul>");
 
 
-$('#saapSlider').bxSlider({
-    auto: true,
-    autoControls: true
-  });
+	$('#saapSlider').bxSlider({
+		auto: true,
+		autoControls: true
+	  });
+}
--- background.js
+++ background.js
@@ -3,6 +3,29 @@
 var iconColor="green", tabId = -1, port;
 
 
+// send a request to the content script
+chrome.tabs.getSelected(null, function(tab) {
+  chrome.tabs.sendRequest(tab.id, {greeting: "hello"}, function(response) {
+    console.log("background 2 " + response.farewell);
+  });
+});
+
+
+
+// listen for requests coming from content scripts
+chrome.extension.onRequest.addListener(
+  function(request, sender, sendResponse) {
+    console.log("background 1 " + (sender.tab ?
+                "from a content script: " + sender.tab.url :
+                "from the extension"));
+    if (request.greeting == "hello")
+      sendResponse({farewell: "b3 goodbye"});
+    if (request.greeting == "iconColor")
+      sendResponse({farewell: iconColor});
+});
+
+
+
 function changeIcon() {
 	if (iconColor === "green") {
 		chrome.browserAction.setIcon({path: "iconRed.png"});
@@ -21,9 +44,27 @@ function changeIcon() {
 // Called when the user clicks on the browser action icon.
 chrome.browserAction.onClicked.addListener(changeIcon); 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //console.log('2');
-// find the tabs having imdb running
+// find the tabs with imdb running
 chrome.tabs.query({}, function(tabArray){
 	//console.log('4');
 	// of those tabs, find the one running Alelie
@@ -32,8 +73,7 @@ chrome.tabs.query({}, function(tabArray){
 		if ((tabArray[i].title === 'Amélie (2001) - IMDb')
 		 || (tabArray[i].title === "Ocean's Eleven (2001) - IMDb")){
 			tabId = i;
-			//console.log('5');
-
+			console.log('5');
 			// open a connection to a content script on a particular tab at a particular port
 			port = chrome.tabs.connect(tabId, {name: 'activeOrNot'});
 			console.log('tabId: ' + tabId + ' port: ' + port.name);
@@ -46,6 +86,6 @@ chrome.tabs.query({}, function(tabArray){
 		}
 	}
 });
-
+*/
 
 //console.log('3');
