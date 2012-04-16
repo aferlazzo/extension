f9e99ddb3b96450c3e03037ba9cf9bbc52d01bed
Anthony Ferlazzo <anthony.ferlazzo@infostretch.com>
2012-04-15 17:32:18 -0700

    messaging is not quite debugged

--- GCvisitInvite.js
+++ GCvisitInvite.js
@@ -1,10 +1,35 @@
-//alert('visitInvite.js start');
-
+console.log('visitInvite.js start');
 
 // Content scripts are JavaScript files that run in the context of web pages.
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// this is for messaging between the content script and the rest of the extension
+
+// Here's what it looks like to respond to incoming connections from a background script
+ 
+// This connect method attempts to connect to other listeners, primarily used by 
+// content scripts connection to other parts of the extension, and assigns a port to 
+// be used for this particular communication
+
+// In order to handle incoming connections set up a onConnect event listener
+console.log('setting up chrome.extension.onConnect.addListener callback function');
+chrome.extension.onConnect.addListener(function(port) {
+	// once a connect request is received, define a listener to process the messages
+	port.onMessage.addListener(function(msg) {
+		if (msg.colorIs == 'green'){
+			ExtenstionActivate= true;
+		} else {
+			ExtenstionActivate= false;
+		}
+		console.log('in content script. msg.colorIs: '+msg.colorIs); 
+	});
+});
+
+//----------------------------------
+
 var articleParent = document.getElementById('maindetails_center_bottom');
-var myArticle = document.createElement("div");
+var myArticle = document.createElement('div');
 
 // Get a reference to the first child and in sert our article here
 var theFirstChild = articleParent.firstChild;
--- background.js
+++ background.js
@@ -1,26 +1,51 @@
-/*
- * Chrome extension Sabre SAAP
- */
-//chrome.tabs.insertCSS(integer tabId, object details, function callback)
-// defaults to current tab
+/* background.js */
+//console.log('1');
+var iconColor="green", tabId = -1, port;
 
-// set background of current tab to red
-//chrome.tabs.insertCSS({code:"document.body.style.background='red !important'"});
 
-//chrome.tabs.insertCSS({code:"document.body.style.background='red !important'"});
+function changeIcon() {
+	if (iconColor === "green") {
+		chrome.browserAction.setIcon({path: "iconRed.png"});
+		chrome.browserAction.setTitle({ title: "SAAP Prototype OFF"});
+		iconColor="red";
+	} else {
+		chrome.browserAction.setIcon({path: "iconGreen.png"});
+		chrome.browserAction.setTitle({ title: "SAAP Prototype ON"});
+		iconColor="green";
+	}
+}
 
-//alert('creating a new tab');
-chrome.tabs.create({
-					index:1,		//make this the second tab
-					url: 'http://www.imdb.com/title/tt0211915/' 
-					});
 
-// inject javascript found in the file into the imdb.com page
-chrome.tabs.executeScript(null, {file: 'visitInvite.js'});
 
-// Fired when a tab is created. Lists the changes to the state of the tab that was updated.
-/*
-chrome.tabs.onCreated.addListener(function(null, url('http://www.imdb.com') {
-		display.log('tab created');
-		});
-*/
+
+// Called when the user clicks on the browser action icon.
+chrome.browserAction.onClicked.addListener(changeIcon); 
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//console.log('2');
+// find the tabs having imdb running
+chrome.tabs.query({}, function(tabArray){
+	//console.log('4');
+	// of those tabs, find the one running Alelie
+	for (var i = 0; i < tabArray.length; i++){
+		//console.log('tab '+ i + ' is has title |' +tabArray[i].title + '|');
+		if ((tabArray[i].title === 'Amélie (2001) - IMDb')
+		 || (tabArray[i].title === "Ocean's Eleven (2001) - IMDb")){
+			tabId = i;
+			//console.log('5');
+
+			// open a connection to a content script on a particular tab at a particular port
+			port = chrome.tabs.connect(tabId, {name: 'activeOrNot'});
+			console.log('tabId: ' + tabId + ' port: ' + port.name);
+			// This is for messaging between this background script and the content script, which interacts with the true web page
+			// and the event is fired when a connection is made from the content script
+
+			// tell content script the iconColor
+			console.log('6');
+			port.postMessage({colorIs: iconColor});
+		}
+	}
+});
+
+
+//console.log('3');
